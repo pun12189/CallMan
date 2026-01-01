e565a1a91ebc4378b24a7471e164b7705b2d02e8
puneet <puneet@sofster.com>
2026-01-01 14:38:43 +0530

    amendments

--- Services/BaseDataService.cs
+++ Services/BaseDataService.cs
@@ -15,12 +15,12 @@ namespace BahiKitab.Services
 #if DEBUG
 
             //_connectionString = "DataSource=bahikitab-aws.c3s6wewcwox1.us-east-1.rds.amazonaws.com;Port=3306;Uid=admin;Pwd=Il6oOvguA2SB5IEQxWCJ;database=bahikitab";
-            _connectionString = "Server=localhost;Uid=root;Pwd='';database=bahikitab";
+            _connectionString = "Server=82.29.166.165;Uid=root;Pwd=sofricdev;database=bahikitabdev";
 #endif
 #if RELEASE
 
             //_connectionString = "Server=192.168.1.90;Uid=cosdb;Pwd=Cosmetify@123;database=cosmetify";
-            _connectionString = "Server=localhost;Uid=root;Pwd='';database=bahikitab";
+            _connectionString = "Server=82.29.166.165;Uid=root;Pwd=sofricprod;database=bahikitabprod";
 #endif
 #if TESTING
 
